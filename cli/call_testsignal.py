149f5e0e95136a56fa8aafa0edc073fa5ab32512
WeiJin <egn0130@gmail.com>
2019-12-18 19:14:32 +0800

    add command guide in call_testsignal.py

--- cli/call_testsignal.py
+++ cli/call_testsignal.py
@@ -1,6 +1,11 @@
 import os
 
+# to see the script.py running process id(print $2)/ username(print $1), "head -1" means only return first pid
 # os.system("ps -ef | grep testsignal.py | awk '{print $2}' | head -1 ")
+
+# to kill python script via the script name
+# os.system("pkill -9 -f script.py")
+
 # os.system("ps -ef | grep analyseface.py | head -1| awk '{print $2}' | xargs kill -USR1 ")
 os.system("ps -ef | grep analyseface.py | head -1| awk '{print $2}' | xargs kill -USR2 ")
 
